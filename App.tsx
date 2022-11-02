2860e764fb86a592654e163ff513c3ce5fbba814
Allisson Lima <Allysoncosta@outlook.com>
2022-11-02 18:06:52 -0300

    feat: add init commit

---
+++ App.tsx
@@ -0,0 +1,31 @@
+import { NativeBaseProvider, StatusBar } from "native-base";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { THEME } from "./src/styles/theme";
+import { Loading } from "./src/components";
+import { SignIn } from "./src/screens/SignIn";
+import { AuthContextProvider } from "./src/context/AuthContext";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  });
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <AuthContextProvider>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        {fontsLoaded ? <SignIn /> : <Loading />}
+      </AuthContextProvider>
+    </NativeBaseProvider>
+  );
+}
